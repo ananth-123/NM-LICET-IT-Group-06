b472ca7a657f3eb978bf665e5466b9fe6643229a
Ananth <ananth123praburaj@gmail.com>
2023-11-24 00:11:45 +0530

    UI Components update
    Co-authored-by: afridariff <afridariff@users.noreply.github.com>
    Co-authored-by: Samsson R <Samsson56@users.noreply.github.com>
    Co-authored-by: Crispin <crispinr@users.noreply.github.com>

--- fe/src/Todo.jsx
+++ fe/src/Todo.jsx
@@ -2,123 +2,297 @@ import { useState } from "react";
 import * as React from "react";
 import { Button } from "./components/Button";
 import { Input } from "./components/Input";
-import { X } from "lucide-react";
+import { X, Calendar as CalendarIcon, Plus } from "lucide-react";
 import { Checkbox } from "./components/Checkbox";
 import { format } from "date-fns";
-import { Calendar as CalendarIcon } from "lucide-react";
+import { useEffect, useCallback } from "react";
+import {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+} from "./components/Select";
 import { cn } from "./lib/utils";
 import { Calendar } from "./components/Calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "./components/Popover";
+import { Label } from "./components/Label";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "./components/Dialog";
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+} from "./components/Table";
+import { Separator } from "./components/Seperator";
 
 const Todo = () => {
   const [todos, setTodos] = useState([]);
   const [todoInput, setTodoInput] = useState("");
   const [deadline, setDeadline] = useState("");
+  const [priority, setPriority] = useState("Normal");
 
   const addTodo = () => {
     if (todoInput.trim() !== "") {
-      setTodos([...todos, { text: todoInput, done: false, deadline }]);
+      setTodos((prev) => {
+        return [
+          ...prev,
+          { text: todoInput, done: false, deadline: date, priority },
+        ];
+      });
       setTodoInput("");
       setDeadline("");
+      setDate("");
+      setPriority(""); // Reset priority
     }
   };
 
   const removeTodo = (index) => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
+    setTodos((prev) => {
+      prev.splice(index, 1);
+      return [...prev];
+    });
   };
 
-  const toggleDone = (index) => {
-    const newTodos = [...todos];
-    newTodos[index].done = !newTodos[index].done;
-    setTodos(newTodos);
-  };
+  const toggleDone = useCallback((index) => {
+    setTodos((prev) => {
+      prev[index].done = !prev[index].done;
+      return [...prev];
+    });
+  }, []);
 
   const isOverdue = (deadline) => {
     return deadline && new Date(deadline) < new Date();
   };
 
-  // const [date, setDate] = React.useState<Date>();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = () => {
     addTodo();
   };
 
+  const handlePriorityChange = (selectedPriority) => {
+    setPriority(selectedPriority);
+  };
+
+  useEffect(() => {
+    setTodos((prev) => {
+      prev
+        .sort((a, b) => (a.done === b.done ? 0 : a.done ? 1 : -1))
+        .sort((a, b) => {
+          const priorityOrder = { Low: 2, Normal: 1, High: 0 };
+          return priorityOrder[a.priority] - priorityOrder[b.priority];
+        });
+      return prev;
+    });
+    console.log("wagwan wahts up");
+  }, [addTodo, toggleDone]);
+
+  const getTimeRemaining = (deadline) => {
+    if (!deadline) return "";
+
+    const now = new Date();
+    const deadlineDate = new Date(deadline);
+    const timeDifference = deadlineDate - now;
+    const daysRemaining = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+
+    if (daysRemaining === 0) {
+      return "Today";
+    } else if (daysRemaining === 1) {
+      return "Tomorrow";
+    } else if (daysRemaining < 0) {
+      return "Overdue";
+    } else {
+      return `${daysRemaining} days left`;
+    }
+  };
+
+  const [date, setDate] = React.useState();
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-6 rounded-lg shadow-ab w-[50%]">
-        <h1 className="text-2xl font-bold mb-4">Todo List</h1>
-
-        <form onSubmit={handleSubmit}>
-          <div className="flex w-full max-w-sm items-center space-x-2">
-            <Input
-              type="text"
-              placeholder="Add a new todo"
-              value={todoInput}
-              onChange={(e) => setTodoInput(e.target.value)}
-            />
-            {/* <Popover>
-              <PopoverTrigger asChild>
-                <Button
-                  variant={"outline"}
-                  className={cn(
-                    "w-[280px] justify-start text-left font-normal",
-                    !date && "text-muted-foreground"
-                  )}
-                >
-                  <CalendarIcon className="mr-2 h-4 w-4" />
-                  {date ? format(date, "PPP") : <span>Pick a date</span>}
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto p-0">
-                <Calendar
-                  mode="single"
-                  selected={date}
-                  onSelect={setDate}
-                  initialFocus
-                />
-              </PopoverContent>
-            </Popover> */}
+    <>
+      <div className="z-10 min-h-screen bg-white">
+        <nav className="z-20 p-4">
+          <div className="container mx-auto">
+            <h1 className="text-2xl font-bold">Todo List</h1>
           </div>
-          <ul>
-            {todos.map((todo, index) => (
-              <li
-                key={index}
-                className={`flex justify-between items-center border-b py-2 ${
-                  todo.done && "line-through"
-                } ${isOverdue(todo.deadline) ? "text-red-500" : ""}`}
-              >
-                <span>
-                  <Checkbox
-                    id="terms"
-                    checked={todo.done}
-                    onChange={() => toggleDone(index)}
-                  />
-
-                  <span className="ml-2 text-md font-medium leading-none">
-                    {todo.text}
-                  </span>
-                  {todo.deadline && (
-                    <span className="ml-2">
-                      Deadline: {new Date(todo.deadline).toLocaleDateString()}
-                    </span>
-                  )}
-                </span>
-                <Button onClick={() => removeTodo(index)} variant="ghost">
-                  <X className="p-1" />
-                </Button>
-              </li>
-            ))}
-          </ul>
-
-          <Button type="submit" className="mt-4">
-            Add Todo
-          </Button>
-        </form>
+        </nav>
+        <Separator className="" />
+        <div className="container mx-auto p-6">
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline">
+                <Plus size={"16"} className="mr-1" />
+                Add Task
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>New Task</DialogTitle>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-1 items-center">
+                  <div className=" rounded-lg">
+                    <div class="relative">
+                      <Input
+                        type="text"
+                        id="taskName"
+                        name="taskName"
+                        className="peer bg-transparent h-10 w-full rounded-lg text-gray-800 placeholder-transparent ring-2 px-2 ring-gray-500 focus:ring-sky-600 focus:outline-none focus:border-rose-600"
+                        placeholder="Task Name"
+                        value={todoInput}
+                        onChange={(e) => setTodoInput(e.target.value)}
+                      />
+                      <label
+                        for="taskName"
+                        className="absolute cursor-text left-0 -top-3 text-sm text-gray-500 bg-white mx-1 px-1 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-sky-600 peer-focus:text-sm transition-all"
+                      >
+                        Task Name
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div className="grid grid-cols-1 items-center">
+                  <Select
+                    value={priority}
+                    onValueChange={(val) => handlePriorityChange(val)}
+                  >
+                    <SelectTrigger className="w-auto">
+                      <SelectValue placeholder="Select a priority" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Priority</SelectLabel>
+                        <SelectItem value="Low">Low</SelectItem>
+                        <SelectItem value="Normal">Normal</SelectItem>
+                        <SelectItem value="High">High</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+                <div className="grid grid-cols-1 items-center mx-full gap-1">
+                  <Label htmlFor="datepick" className="text-left">
+                    Choose Deadline
+                  </Label>
+                  <Popover>
+                    <PopoverTrigger className="w-full" asChild>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-full justify-start text-left font-normal",
+                          !date && "text-muted-foreground"
+                        )}
+                      >
+                        <CalendarIcon className="mr-2 h-4 w-4" />
+                        {date ? format(date, "PPP") : <span>Pick a date</span>}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0">
+                      <Calendar
+                        mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </div>
+              </div>
+              <DialogFooter>
+                <DialogClose>
+                  <Button
+                    type="submit"
+                    className=""
+                    onClick={() => {
+                      handleSubmit();
+                    }}
+                  >
+                    Create Task
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+          {todos.length > 0 && (
+            <div className="mb-6">
+              <Table className="">
+                <TableHeader>
+                  <TableRow className="">
+                    <TableHead className="w-[5%]">Done</TableHead>
+                    <TableHead className="w-[5%]">S.No</TableHead>
+                    <TableHead className="w-[35%]">Task</TableHead>
+                    <TableHead className="w-[15%]">Priority</TableHead>
+                    <TableHead className="">Deadline</TableHead>
+                    <TableHead className="">Status</TableHead>
+                    <TableHead className=""></TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {todos.map((todo, index) => (
+                    <TableRow
+                      key={index}
+                      className={`${index % 2 === 0 ? "bg-gray-100" : ""} ${
+                        isOverdue(todo.deadline) ? "text-red-500 font-bold" : ""
+                      } `}
+                    >
+                      <TableCell className="mx-auto">
+                        <Checkbox
+                          id={`task-${index}`}
+                          checked={todo.done}
+                          onCheckedChange={() => {
+                            toggleDone(index);
+                          }}
+                        />
+                      </TableCell>
+                      <TableCell className={`${todo.done && "line-through"}`}>
+                        {index + 1}
+                      </TableCell>
+                      <TableCell className={`${todo.done && "line-through"}`}>
+                        {todo.text}
+                      </TableCell>
+                      <TableCell className={`${todo.done && "line-through"}`}>
+                        {todo.priority}
+                      </TableCell>
+                      <TableCell className={`${todo.done && "line-through"}`}>
+                        {todo.deadline
+                          .toJSON()
+                          .slice(0, 10)
+                          .split("-")
+                          .reverse()
+                          .join("/")}
+                        <div className="text-sm text-gray-500">
+                          {getTimeRemaining(todo.deadline)}
+                        </div>
+                      </TableCell>
+                      <TableCell>{todo.done ? "Done" : "Pending"}</TableCell>
+                      <TableCell className="float-right">
+                        <Button
+                          onClick={() => removeTodo(index)}
+                          variant="ghost"
+                          className="ml-2"
+                        >
+                          <X />
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -1,4 +1,8 @@
 const { fontFamily } = require("tailwindcss/defaultTheme");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+const svgToDataUri = require("mini-svg-data-uri");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -75,5 +79,19 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "bg-grid": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 };
